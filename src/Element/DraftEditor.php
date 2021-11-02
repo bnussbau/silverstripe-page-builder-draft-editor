d162799b8fcc0fb5401402f6b6ec50c9e2f410ff
Zauberfisch <zauberfisch@zauberfisch.at>
2021-11-02 03:20:38 +0100

    getValueForFrontend

--- src/Element/DraftEditor.php
+++ src/Element/DraftEditor.php
@@ -7,5 +7,15 @@ namespace zauberfisch\PageBuilderDraftEditor\Element;
 use zauberfisch\PageBuilder\Element\Element;
 
 class DraftEditor extends Element {
-
+	public function getValueForFrontend(): array {
+		$element = $this->array;
+		if (isset($element['props']['content']['entityMap'])) {
+			foreach ($element['props']['content']['entityMap'] as &$entry) {
+				if ($entry['type'] === 'LINK') {
+					$entry['data'] = $this->frontendConvertLink($entry['data']);
+				}
+			}
+		}
+		return $element;
+	}
 }
