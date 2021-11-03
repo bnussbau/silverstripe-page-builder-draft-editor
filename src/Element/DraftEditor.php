48e18767532ede743c2250d51dfd141380b88317
Zauberfisch <zauberfisch@zauberfisch.at>
2021-11-03 07:44:09 +0100

    update

--- src/Element/DraftEditor.php
+++ src/Element/DraftEditor.php
@@ -7,12 +7,12 @@ namespace zauberfisch\PageBuilderDraftEditor\Element;
 use zauberfisch\PageBuilder\Element\Element;
 
 class DraftEditor extends Element {
-	public function getValueForFrontend(): array {
-		$element = $this->array;
-		if (isset($element['props']['content']['entityMap'])) {
-			foreach ($element['props']['content']['entityMap'] as &$entry) {
-				if ($entry['type'] === 'LINK') {
-					$entry['data'] = $this->frontendConvertLink($entry['data']);
+	public function getValueForFrontend() {
+		$element = parent::getValueForFrontend();
+		if (isset($element->props->content->entityMap)) {
+			foreach ($element->props->content->entityMap as &$entry) {
+				if ($entry->type === 'LINK') {
+					$entry->data = $this->frontendConvertLink($entry->data);
 				}
 			}
 		}
